55ccfb746c33de6e52be8cdc2a19cecfb379461c
Tom Ward <wardusmcbay@gmail.com>
2024-10-28 20:35:37 +0000

    installed middleware packages, initalised port, connected to mongodb, building home page

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+    food:       { type: String, required: true },
+    amount:     { type: String, required: true },
+    useByDate:  { type: String, required: true },
+    foodGroup:  { type: String },
+    calories:   { type: String },
+    DietaryReqs: {
+        type: String,
+        enum: [none, lactose-free, vegan, vegetarian],
+    }
+});
+
+const userSchema = mongoose.Schema({
+    username: { type: String, required: true},
+    password: { type: String, required: true},
+    foods: [foodSchema],
+});
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+app.use(express.urlencoded({ extended: false}));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+});
+
